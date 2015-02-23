hawkw/cs383f2015-lab3

commit 6e2b0c7ba1e2bedd37e4a9a9297becc48e116f4f

Add ability to specify maximum run time (in ticks) This is so the batch runs can all be terminated

ElFarol/src/elfarol/AgentMarkov.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
  * Agent that predicts bar attendance using a Markov Chain, mapping previous
  * attendance to expected attendance
  * @author Willem Yarbrough <yarbroughw@allegheny.edu>
- * @author Luke Smith <smithl4@allegheny.edu>
+ * 		   Luke Smith <smithl4@allegheny.edu>
  *
  */
 public class AgentMarkov implements Agent {

ElFarol/src/elfarol/ElFarolContextBuilder.java
@@ -52,7 +52,7 @@ public class ElFarolContextBuilder extends DefaultContext<Object> implements
 			context.add(agent);
 		}
 		
-		for (int i = 0; i < agentAmount( ParameterWrapper.getPercentSmart(),
+		for (int i = 0; i < agentAmount( ParameterWrapper.getPercentAvg(),
 				ParameterWrapper.getAgentsNumber() ); i++) {
 			final Agent agent = new AgentMarkov(); 
 			context.add(agent);
@@ -75,6 +75,9 @@ public class ElFarolContextBuilder extends DefaultContext<Object> implements
 				}
 				context.add(agent);
 			}
+		}
+		if (getRunTicks() > -1) {
+			RunEnvironment.getInstance().endAt(getRunTicks());
 		}
 		return context;
 	}

ElFarol/src/elfarol/ParameterWrapper.java
@@ -51,6 +51,8 @@ public final class ParameterWrapper {
 
 	/** Specifies the threshold level. */
 	private static int overcrowdingThreshold = 60;
+	
+	private static int runTicks = -1;
 
 	// ========================================================================
 	// === Getter Methods =====================================================
@@ -71,6 +73,10 @@ public final class ParameterWrapper {
 	}
 	public static int getPercentSmart() {
 		return percentSmart;
+	}
+
+	public static int getRunTicks() {
+		return runTicks;
 	}
 
 	/**
@@ -132,6 +138,8 @@ public final class ParameterWrapper {
 
 		overcrowdingThreshold = ((Integer) parameters
 				.getValue("overcrowdingThreshold")).intValue();
+		runTicks = ((Integer) parameters
+				.getValue("runTicks")).intValue();
 	}
 
 	// ========================================================================

commit 4143ba205580d2b71f6ec2cc6eaf01228552c196

Fixed issues accidentally introduced in 6e2b0c7ba1e2bedd37e4a9a9297becc48e116f4f

ElFarol/src/elfarol/AgentMarkov.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
  * Agent that predicts bar attendance using a Markov Chain, mapping previous
  * attendance to expected attendance
  * @author Willem Yarbrough <yarbroughw@allegheny.edu>
- * 		   Luke Smith <smithl4@allegheny.edu>
+ * @author Luke Smith <smithl4@allegheny.edu>
  *
  */
 public class AgentMarkov implements Agent {

ElFarol/src/elfarol/ElFarolContextBuilder.java
@@ -52,7 +52,7 @@ public class ElFarolContextBuilder extends DefaultContext<Object> implements
 			context.add(agent);
 		}
 		
-		for (int i = 0; i < agentAmount( ParameterWrapper.getPercentAvg(),
+		for (int i = 0; i < agentAmount( ParameterWrapper.getPercentSmart(),
 				ParameterWrapper.getAgentsNumber() ); i++) {
 			final Agent agent = new AgentMarkov(); 
 			context.add(agent);